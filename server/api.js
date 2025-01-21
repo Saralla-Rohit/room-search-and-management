a81057e334d4de15d0fc528bb121e67d60619527
rohit sarla <rohitsarla69@gmail.com>
2025-01-21 14:04:20 +0530

    fixed filter cors issue

--- server/api.js
+++ server/api.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://room-search-and-management-1.onrender.com'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://room-search-and-management.onrender.com'],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
     credentials: true
